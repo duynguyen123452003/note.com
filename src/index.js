44da256d86d7f3795a9a71c936d9d0ac0dba7438
dddecevesverv <duynguyenst15@gmail.com>
2023-09-13 15:01:47 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Global from './Components/Global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Global>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Global>,
 );
 
 // If you want to start measuring performance in your app, pass a function
